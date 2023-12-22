e6d91a7864c38b041238d14e5dd680b27d6d97ef
bnjlka <dolwans12@gmail.com>
2023-12-22 13:55:16 +0300

    чуть улучшил

--- bot.py
+++ bot.py
@@ -24,83 +24,80 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 #Пустой список для очереди
 queue = []
+names_video = []
 loop_on = False
 
 ffmpeg_options = {
                 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
                 'options': '-vn',
                 }
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.command()
-async def play(ctx, url):
-    voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    channel = ctx.author.voice.channel
-    # Есть ли бот в каком-либо канале
-    if voice_channel is None: #Бота нет ни в одном канале
-        await channel.connect()
-        voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    elif str(voice_channel.channel.name).strip() is not str(channel).strip(): #Бот в другом канале
-        await voice_channel.disconnect()
-        await channel.connect()
-
-    ydl = yt_dlp.YoutubeDL({
+ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
 		'key': 'FFmpegExtractAudio',
 		'preferredcodec': 'mp3',
 		'preferredquality': '192',
 		}],
-	})
+	}
 
+
+ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+@bot.command()
+async def connect(ctx):
+    voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    channel = ctx.author.voice.channel
+    if voice_channel is None: #Бота нет ни в одном канале
+        await channel.connect()
+    elif str(voice_channel.channel.name).strip() is not str(channel).strip(): #Бот в другом канале
+        await voice_channel.disconnect()
+        await channel.connect()
+
+@bot.command()
+async def play(ctx, url):
     global queue
+    global ydl  
     
-    try:
-        info = ydl.extract_info(url, download=False)
-    except yt_dlp.utils.DownloadError:
-        try:
-            await splay(ctx, url = url)
-            return
-        except spotipy.exceptions.SpotifyException:
-            await saplay(ctx, url = url)
-            return
-
-    try:
-        for urls in info['entries']:
-            queue.append(urls['webpage_url'])
-    except KeyError:
-        queue.append(url)
+    await get_url(ctx, url = url)
+    await connect(ctx)
     voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if not voice_channel.is_playing(): #Если он ничего не играет
         await next(ctx)
         return
- 
+
+async def get_url(ctx, url):
+    global ydl
+    try:
+        info = ydl.extract_info(url, download=False)
+        try:
+            for urls in info['entries']:
+                queue.append(urls['url'])
+                names_video.append(urls['title'])
+        except KeyError:
+            queue.append(info['url'])
+            names_video.append(info['title'])
+    except yt_dlp.utils.DownloadError:
+        try:
+            await splay(ctx, url = url)
+        except spotipy.exceptions.SpotifyException:
+            await saplay(ctx, url = url)
+
 async def next(ctx):
     global queue
-    if queue:
-        ydl = yt_dlp.YoutubeDL({
-            'format': 'bestaudio/best',
-		    'postprocessors': [{
-		    'key': 'FFmpegExtractAudio',
-		    'preferredcodec': 'mp3',
-		    'preferredquality': '192',
-		    }],
-	    })
+    global ydl
 
+    if queue: 
         voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
         # Используем yt-dlp для получения музыкальной информации
         url = queue.pop(0)
-        info = ydl.extract_info(url, download=False)
-        url2 = info['url']
         #Достаем название ссылки и пишем в чате
-        video_title = info['title']
+        video_title = names_video.pop(0)
         await ctx.send(f'Играет: {video_title}')
-        
-        voice_channel.play(discord.FFmpegPCMAudio(url2, **ffmpeg_options), after=lambda e: for_loop(ctx,url2))
-
+        voice_channel.play(discord.FFmpegPCMAudio(url, **ffmpeg_options), after=lambda e: for_loop(ctx,url))
         #Ждем пока играет
         while voice_channel.is_playing():
             await asyncio.sleep(1)
@@ -157,16 +154,7 @@ async def loop(ctx):
 async def search(ctx, *, name_song):
     channel = ctx.author.voice.channel
     voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    
-    ydl = yt_dlp.YoutubeDL({
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-	    'key': 'FFmpegExtractAudio',
-	    'preferredcodec': 'mp3',
-        'preferredquality': '192',
-	    }],
-        'noplaylist': True,
-    })
+     
     search_results = ydl.extract_info(f'ytsearch5:{name_song}', download = False)['entries']
 
     if not search_results:
@@ -185,12 +173,18 @@ async def search(ctx, *, name_song):
         response = await bot.wait_for('message', check=check, timeout=30)
         selected_index = int(response.content) - 1
         selected_video = search_results[selected_index]
-        video_url = selected_video['webpage_url']
+        video_url = selected_video['url']
+        name_video = selected_video['title']
+        queue.append(video_url)
+        names_video.append(name_video)
+        await connect(ctx)
     except (ValueError, IndexError, asyncio.TimeoutError):
         await ctx.send("Поздно или не то")
         return
+    if not voice_channel.is_playing():
+        await next(ctx)
+        return
 
-    await play(ctx,video_url)
 
 async def splay(ctx, url):
     name_track = sp.track(url)['name']
@@ -200,35 +194,24 @@ async def splay(ctx, url):
 
 async def search_song(ctx, *, name_song):
     global queue
+    global ydl
     channel = ctx.author.voice.channel
     voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    
-    ydl = yt_dlp.YoutubeDL({
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-	    'key': 'FFmpegExtractAudio',
-	    'preferredcodec': 'mp3',
-        'preferredquality': '192',
-	    }],
-        'noplaylist': True,
-    })
     search_video = ydl.extract_info(f'ytsearch1:{name_song}', download = False)['entries']
-    video_url = search_video[0]['webpage_url']
-    
+    video_url = search_video[0]['url']
+    name_video = search_video[0]['title']
     queue.append(video_url)
+    names_video.append(name_video)
 
 async def saplay(ctx, url):
     album_info = sp.album_tracks(url)
     urls_from_album_info = album_info['items'][0]['external_urls']['spotify']
     voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     for url in album_info['items']:
-        #print(url['external_urls']['spotify'])
         await splay(ctx, url = url['external_urls']['spotify'])
     if not voice_channel.is_playing(): #Если он ничего не играет
-       await next(ctx)
-       return
-    
-    #print(urls_from_album_info)
+        await next(ctx)
+        return
 
 #Запускаем бота
 bot.run(bot_token)
